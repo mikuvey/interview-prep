d99c25474b1b678bb309302bcc66f5789f36d158
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-04-06 21:26:11 -0700

    ContentUpdate: Java Qs

---
+++ java/University.java
@@ -0,0 +1,33 @@
+package java;
+
+import java.util.*;
+
+class University{
+    private String name;
+    private String address;
+    private int numStudents;
+    private int numStaff;
+    
+    // public void setUnivName(String name){
+    //     this.name = name;
+    // }
+
+    // public void setAddress(String address){
+    //     this.address = address;
+    // }
+
+    // public void setStudents(int numStudents){
+    //     this.numStudents = numStudents;
+    // }
+
+    // public void setStaff(int numStaff){
+    //     this.numStaff = numStaff;
+    // }
+
+    public 
+
+
+    public static void main(String[] args){
+
+    }
+}
--- java/readme.md
+++ java/readme.md
@@ -1,2 +1,16 @@
 # Java Interview Questions
 
+1. Is Java Heap same as heap data structure?
+2. Why pointers not used in Java?
+3. Is java both compiled as well as Interpreted language?
+4. What's JIT? 
+5. What's Constructor Overloading?
+6. What's copy constructore? Give example.
+7. Method Overloading vs Mehod Overriding?
+8. What do we use final keyword for?
+9. Final vs finally vs finalize?
+10. When can we use super Keyword?
+11. Static vs Final in java?
+12. Why use Garbage Collection?
+13. Shallow copy vs Deep Copy in java? 
+14. Why are Strings immutable in java?
